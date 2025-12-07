61da6e0a8cb7e5a4e28e1dda4618d1f055b909f8
Connor Young <connor.young@vitinitiative.com>
2025-12-06 19:51:06 -0500

    Adding functionality to categorize a transaction in a variety of ways

--- backend/agents/transaction_categorizer_agent.py
+++ backend/agents/transaction_categorizer_agent.py
@@ -1,13 +1,23 @@
 from ddgs import DDGS
 from mistralai import Mistral
 
+from dataclasses import dataclass
+
+@dataclass
+class CategorizationResult:
+    category_id: str
+    confidence_level: float
+
 class OrderCategorizerAgent:
-    def __init__(self, llm_client: Mistral):
+    def __init__(self, llm_client: Mistral, categories: List[str]):
         mistral_client = llm_client
         model = "mistral-medium-latest"
+        categories = categories
         pass
 
-    def categorize_transaction_by_payee_name(self, payee_name: str):
+    def categorize_transaction(self, transaction: Transaction) -> None:
+        self.__categorize_transaction_by_payee_name(payee_name=transaction.payee_name)
+    def __categorize_transaction_by_payee_name(self, payee_name: str):
         self.llm_client.chat.complete(
             model=self.model,
             messages=[
@@ -22,10 +32,34 @@ class OrderCategorizerAgent:
                 },
                 {
                     "role": "user",
-                    "content": ""
+                    "content": "categorizes: f{JSON.stringify(categories)} payee_name: {payee_name}"
                 }
-            ]
+            ],
+            temperature=0.0
         )
 
-    def search_payee_name_on_internet(payee_name):
-        DDGS().text(payee_name, max_results=3)
+    def categorize_internet(self, transaction: Transaction):
+        self.llm_client.chat.complete(
+            model=self.model,
+            messages=[
+                {
+                    "system": "system",
+                    "content": """
+                    You are a data labeller for a credit card company. You will be given all the possible categories that a transaction can be in the form of JSON.
+                    You will also be given the search results for the payee name on the internet. By looking at the different search results, you want to determine
+                    what the category of the transaction is. If addition to your categorization, you will also provide a confidence level which will denote how likely this categorization is.
+                    """
+                },
+                {
+                    "role": "user",
+                    "content": ""
+                }
+            ],
+            temperature=0.0
+        )
+
+    def __search_payee_name_on_internet(payee_name):
+        results = DDGS().text(payee_name, max_results=3)
+        results.get_text()
+        
+        return results
--- backend/worker.py
+++ backend/worker.py
@@ -1,20 +1,28 @@
+import schedule
+
 from clients.ynab_client import YNABClient
 from config import settings
 from services.ynab_service import YNABService
+from agents.categorize_transaction_agent import CategorizeTransactionAgent
 
 def main():
     ynab_client = YNABClient(access_token=settings.ynab_access_key)
     ynab_service = YNABService(ynab_client=ynab_client)
+    categorize_transaction_agent = CategorizeTransactionAgent(ynab_service=ynab_service)
 
+def categorize_latest_transactions(
+    categorize_transaction_agent: CategorizeTransactionAgent
+):
     transactions = ynab_service.get_transactions()
 
     for t in transactions:
         if not t.category_id:
-            # Need to categorize this transaction
-            pass
-
-def categorize_latest_transactions():
+            categorize_transaction_agent.categorize_transaction(transaction=t)
     
 
 if __name__ == "__main__":
-    main()
+    schedule.every(1).day.do(main)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(120)
